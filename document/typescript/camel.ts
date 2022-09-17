443ae37766613ee32af760a4f91f884d1efb3acc
maqiqian <1363964820@qq.com>
2022-09-17 11:53:21 +0800

    docs(type): CamelCase

---
+++ document/typescript/camel.ts
@@ -0,0 +1,33 @@
+/**
+ * 实现工具类型 A => B 的转化
+ */
+
+type A = {
+  aa: string;
+  Bb: string;
+  Dc_Dd: {
+    BB: string;
+    Dd_Db_DA_DC: string;
+  };
+};
+
+type B = {
+  aa: string;
+  bb: string;
+  ccDd: {
+    bb: string;
+    ddDbDaDc: string;
+  };
+};
+
+type Camel<T> = T extends any ? {
+  [K in keyof T as CamelCase<K & string>]: T[K] extends Record<string, string>
+  ? Camel<T[K]>
+  : T[K]
+} : never
+
+type CamelCase<T extends string> = T extends `${infer L}_${infer R}`
+  ? `${Lowercase<L>}${Capitalize<CamelCase<R>>}`
+  : Lowercase<T>;
+
+type C = Camel<A>
